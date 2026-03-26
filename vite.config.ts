029ac4d91a094702ae1c3cc3dbdb996a9f06d49e
Vinicius-Bedeschi <viniciusbedesch@gmail.com>
2026-03-25 22:52:23 -0300

    Correção

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
@@ -28,10 +27,10 @@ export default defineConfig(({ mode }) => ({
         icons: [{ src: "/favicon.ico", sizes: "64x64", type: "image/x-icon" }],
       },
     }),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
